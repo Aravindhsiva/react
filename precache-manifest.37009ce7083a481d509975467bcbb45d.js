5ba78a0b1db6fb876e0d257d00b9b230509491bc
Aravindhsiva <adamaravindh@gmail.com>
2018-11-12 19:18:11 +0530

    Updates

---
+++ precache-manifest.37009ce7083a481d509975467bcbb45d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "bf2515a31126aef78a60",
+    "url": "/react/static/js/runtime~main.bf2515a3.js"
+  },
+  {
+    "revision": "8f267dd5c8b387d05cf3",
+    "url": "/react/static/js/main.8f267dd5.chunk.js"
+  },
+  {
+    "revision": "1a2fe23bbee4e72efb16",
+    "url": "/react/static/js/1.1a2fe23b.chunk.js"
+  },
+  {
+    "revision": "8f267dd5c8b387d05cf3",
+    "url": "/react/static/css/main.1bcf20fd.chunk.css"
+  },
+  {
+    "revision": "6a55adb9be486bb4bb6d4a16ecc57b5f",
+    "url": "/react/index.html"
+  }
+];
